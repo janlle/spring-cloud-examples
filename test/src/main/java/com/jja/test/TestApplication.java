bae00fcc2ceac917dfd08eee75fa3202a08b6f99
think <rethinkt@163.com>
2018-01-19 18:20:21 +0800

    添加初始化信息

--- test/src/main/java/com/jja/test/TestApplication.java
+++ test/src/main/java/com/jja/test/TestApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class TestApplication {
 
 	public static void main(String[] args) {
+		System.out.println("系统正在启动。。。");
 		SpringApplication.run(TestApplication.class, args);
 	}
 }
