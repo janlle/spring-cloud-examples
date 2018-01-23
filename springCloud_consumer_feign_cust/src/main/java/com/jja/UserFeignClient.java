924714072e651e29f4a97f738b1cad8aed2bc896
jack <rethinkt@163.com>
2018-01-23 16:34:35 +0800

    修改代码

--- springCloud_consumer_feign_cust/src/main/java/com/jja/UserFeignClient.java
+++ springCloud_consumer_feign_cust/src/main/java/com/jja/UserFeignClient.java
@@ -1,5 +1,6 @@
 package com.jja;
 
+import com.config.ConfigurationFei1;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,13 +8,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.config.ConfigurationFei;
 import com.jja.entity.Users;
 
 import feign.Param;
 import feign.RequestLine;
 
-@FeignClient(name = "springCloud-provider", configuration = ConfigurationFei.class)
+@FeignClient(name = "springCloud-provider", configuration = ConfigurationFei1.class)
 public interface UserFeignClient {
 	
 	@RequestLine("GET /getUser/{id}")
