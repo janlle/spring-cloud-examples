5e5eaff75b760267a6bb92baeeae3eedea1d7cb0
leone <janlle@126.com>
2018-01-23 21:48:45 +0800

    修改代码

--- springCloud_consumer_feign_hystrix_wo_hystrix/src/main/java/com/jja/HystrixClientFallback2.java
+++ springCloud_consumer_feign_hystrix_wo_hystrix/src/main/java/com/jja/HystrixClientFallback2.java
@@ -1,10 +1,12 @@
 package com.jja;
 
 import com.jja.feign.FeignClient2;
-
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Component
 public class HystrixClientFallback2 implements FeignClient2 {
 
-	@Override
+    @Override
 	public String findEurekaServiceByName(String serviceName) {
 		return "haha";
 	}
