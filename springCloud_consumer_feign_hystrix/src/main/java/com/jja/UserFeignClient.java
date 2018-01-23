525a49ceaeff39218495477e019824323c4ee2a9
leone <janlle@126.com>
2018-01-23 20:58:43 +0800

    修改代码

--- springCloud_consumer_feign_hystrix/src/main/java/com/jja/UserFeignClient.java
+++ springCloud_consumer_feign_hystrix/src/main/java/com/jja/UserFeignClient.java
@@ -2,8 +2,6 @@ package com.jja;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
