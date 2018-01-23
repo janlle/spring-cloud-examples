73372221160a67dec872e96ac6130ccf2053d679
leone <janlle@126.com>
2018-01-23 21:13:11 +0800

    修改代码

--- springCloud_consumer_feign_hystrix_wo_hystrix/src/main/java/com/jja/HystrixClientFallback1.java
+++ springCloud_consumer_feign_hystrix_wo_hystrix/src/main/java/com/jja/HystrixClientFallback1.java
@@ -2,11 +2,12 @@ package com.jja;
 
 import com.jja.entity.Users;
 
-public class HystrixClientFallback implements UserFeignClient {
+public class HystrixClientFallback1 implements UserFeignClient1 {
 		@Override
 		public Users getUser(int id) {
 			Users user = new Users();
 			user.setId(0);
+			System.out.println("默认姓名1");
 			return user;
 		}
 	}
