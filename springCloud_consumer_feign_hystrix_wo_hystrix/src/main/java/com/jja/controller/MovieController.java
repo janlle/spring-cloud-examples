e00ead5d502bb5da001693af91f27a84a757bd23
leone <exklin@gmail.com>
2018-01-23 21:13:11 +0800

    修改代码

--- springCloud_consumer_feign_hystrix_wo_hystrix/src/main/java/com/jja/controller/MovieController.java
+++ springCloud_consumer_feign_hystrix_wo_hystrix/src/main/java/com/jja/controller/MovieController.java
@@ -3,20 +3,16 @@ package com.jja.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.jja.UserFeignClient;
+import com.jja.UserFeignClient1;
 import com.jja.entity.Users;
 
 @RestController
 public class MovieController {
 	
 	@Autowired
-	private UserFeignClient userFeignClient;
+	private UserFeignClient1 userFeignClient;
 	
 	@GetMapping("/getObj/{id}")
 	public Users getObj(@PathVariable("id")int id) {
