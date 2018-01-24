e4d3f7cf3540179dc53589b3832e3ff3347326e7
jack <rethinkt@163.com>
2018-01-24 14:33:18 +0800

    新建项目

---
+++ springCloud_consumer_feign_hystrix_factory/src/main/java/com/jja/HystrixClientFallback.java
@@ -0,0 +1,15 @@
+package com.jja;
+
+import com.jja.entity.Users;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HystrixClientFallback implements UserFeignClient {
+		@Override
+		public Users getUser(int id) {
+			Users user = new Users();
+			user.setId(0);
+			user.setUsername("默认名称");
+			return user;
+		}
+	}
