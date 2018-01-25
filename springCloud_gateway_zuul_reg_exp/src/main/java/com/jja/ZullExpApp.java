b24dc83d943dc943e55cf96865072e02919d4703
leone <janlle@126.com>
2018-01-25 10:11:47 +0800

    修改代码

--- springCloud_gateway_zuul_reg_exp/src/main/java/com/jja/ZullExpApp.java
+++ springCloud_gateway_zuul_reg_exp/src/main/java/com/jja/ZullExpApp.java
@@ -3,6 +3,8 @@ package com.jja;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -10,5 +12,11 @@ public class ZullExpApp {
 	public static void main(String[] args) {
 		SpringApplication.run(ZullExpApp.class, args);
 		
+	}
+	@Bean
+	public PatternServiceRouteMapper serviceRouteMapper() {
+		return new PatternServiceRouteMapper(
+				"(?<name>^.+)-(?<version>v.+$)",
+				"${version}/${name}");
 	}
 }
