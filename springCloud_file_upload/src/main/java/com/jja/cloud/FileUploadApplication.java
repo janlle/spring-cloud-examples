1d05aef903353292c29f5e9515028e4cfec1e1e8
jack <rethinkt@163.com>
2018-01-25 16:26:16 +0800

    add

---
+++ springCloud_file_upload/src/main/java/com/jja/cloud/FileUploadApplication.java
@@ -0,0 +1,13 @@
+package com.jja.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class FileUploadApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(FileUploadApplication.class, args);
+  }
+}
