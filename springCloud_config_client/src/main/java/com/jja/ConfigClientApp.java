65d56595b1bfb70af7c1f5f083bbe1550d357309
leone <janlle@126.com>
2018-02-07 22:42:51 +0800

    update

--- springCloud_config_client/src/main/java/com/jja/ConfigClientApp.java
+++ springCloud_config_client/src/main/java/com/jja/ConfigClientApp.java
@@ -10,6 +10,4 @@ public class ConfigClientApp {
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientApp.class, args);
 	}
-
-
 }
--- springCloud_config_client/src/main/java/com/jja/controller/ConfigClientController.java
+++ springCloud_config_client/src/main/java/com/jja/controller/ConfigClientController.java
@@ -2,9 +2,13 @@ package com.jja.controller;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class ConfigClientController {
@@ -12,9 +16,20 @@ public class ConfigClientController {
     @Value("${profile}")
     private String profile;
 
+    @Value("${sql}")
+    private String sql;
+
     @RequestMapping(value = "/profile", method = RequestMethod.GET)
-    public String getProfile() {
+    public String  getProfile() {
         return profile;
+    }
+
+    @GetMapping("/env")
+    public Map<String, String> env() {
+        Map<String ,String > map = new HashMap<>();
+        map.put("profile", profile);
+        map.put("sql", sql);
+        return map;
     }
 
 
