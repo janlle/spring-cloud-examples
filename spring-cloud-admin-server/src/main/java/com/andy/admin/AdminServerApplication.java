7a6085a967ed897e0b24bc5a238e01fad09d0662
leone <exklin@gmail.com>
2018-10-11 15:39:59 +0800

    fix(pom):update some module

--- spring-cloud-admin-server/src/main/java/com/andy/admin/AdminServerApplication.java
+++ spring-cloud-admin-server/src/main/java/com/andy/admin/AdminServerApplication.java
@@ -3,6 +3,7 @@ package com.andy.admin;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * <p>
@@ -10,8 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Leone
  * @since 2018-10-11
  **/
-
 @EnableAdminServer
+@EnableEurekaClient
 @SpringBootApplication
 public class AdminServerApplication {
 
