16d23f3219f4c46cfbe6554e1985a203b231097a
leone <exklin@gmail.com>
2018-10-11 11:16:38 +0800

    update

---
+++ email.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="fphd_xian_lufei@fphd"
+CORRECT_NAME="sy-records"
+CORRECT_EMAIL="52o@qq52o.cn"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
