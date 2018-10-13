fbbc1ade499cb6add5344573afde8965efd83bee
leone <exklin@gmail.com>
2018-10-13 18:42:21 +0800

    feat(pom):update

--- spring-cloud-turbine/src/main/java/com/andy/TurbineApplication.java
+++ spring-cloud-turbine/src/main/java/com/andy/TurbineApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
+/**
+ * <p>
+ *
+ * @author Leone
+ * @since 2018-05-13
+ **/
 @EnableTurbine
 @SpringBootApplication
-public class TurbineApp {
+public class TurbineApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(TurbineApp.class, args);
+		SpringApplication.run(TurbineApplication.class, args);
 	}
 }
