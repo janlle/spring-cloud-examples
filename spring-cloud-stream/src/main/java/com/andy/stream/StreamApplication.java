576654d4b99ef092908faa990f012e362245910f
leone <janlle@126.com>
2018-10-13 18:36:27 +0800

    feat(pom):update dashboard module

--- spring-cloud-stream/src/main/java/com/andy/stream/StreamApplication.java
+++ spring-cloud-stream/src/main/java/com/andy/stream/StreamApplication.java
@@ -6,12 +6,14 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author Leone
- * @since 2018-06-23 12:45
+ * @since 2018-06-23
  **/
 @EnableDiscoveryClient
 @SpringBootApplication
-public class StreamApp {
+public class StreamApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(StreamApp.class, args);
+        SpringApplication.run(StreamApplication.class, args);
     }
+
 }
