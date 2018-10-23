3f96c1d9306a1482f7adc5f0cab60e1b7622989b
leone <janlle@126.com>
2018-10-23 19:58:06 +0800

    feat(pom):update

--- spring-cloud-file-upload/src/main/java/com/andy/upload/FileUploadApplication.java
+++ spring-cloud-file-upload/src/main/java/com/andy/upload/FileUploadApplication.java
@@ -1,8 +1,14 @@
-package com.andy.cloud;
+package com.andy.upload;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 
+/**
+ * <p> 
+ *
+ * @author Leone
+ * @since 2018-02-23
+ **/
 @SpringCloudApplication
 public class FileUploadApplication {
     public static void main(String[] args) {
