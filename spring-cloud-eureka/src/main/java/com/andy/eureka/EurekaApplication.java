c626dde25fa3d8f41d9d0319d7c17f96077d1ef2
leone <exklin@gmail.com>
2018-10-25 12:39:29 +0800

    feat(pom):update eureka

--- spring-cloud-eureka/src/main/java/com/andy/eureka/EurekaApplication.java
+++ spring-cloud-eureka/src/main/java/com/andy/eureka/EurekaApplication.java
@@ -3,9 +3,6 @@ package com.andy.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * <p>
@@ -19,14 +16,6 @@ public class EurekaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
-    }
-
-    @EnableWebSecurity
-    static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.csrf().disable();
-        }
     }
 
 }
