b0771456f98a6b3f1edb5dec29b1676d3615aa24
leone <janlle@126.com>
2018-11-08 18:01:26 +0800

    feat(pom):update

--- spring-cloud-feign/src/main/java/com/andy/feign/client/UserFeignClient.java
+++ spring-cloud-feign/src/main/java/com/andy/feign/client/UserFeignClient.java
@@ -1,18 +1,17 @@
 package com.andy.feign.client;
 
+import com.andy.feign.config.ConfigurationFeign1;
 import com.andy.feign.entity.User;
 import feign.Param;
 import feign.RequestLine;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author Leone
  * @since 2018-03-13
  **/
-@FeignClient("USER-SERVICE")
+@FeignClient(name = "spring-cloud-user", configuration = ConfigurationFeign1.class)
 public interface UserFeignClient {
 
 //	不支持getMapping和postMapping并且pathVariable的value属性必须填写
