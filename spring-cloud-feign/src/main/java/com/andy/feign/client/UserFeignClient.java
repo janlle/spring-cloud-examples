7ff04db50b44936ce05ea6225fa387ecbd8f9983
leone <janlle@126.com>
2018-11-10 15:02:01 +0800

    update

--- spring-cloud-feign/src/main/java/com/andy/feign/client/UserFeignClient.java
+++ spring-cloud-feign/src/main/java/com/andy/feign/client/UserFeignClient.java
@@ -1,35 +1,41 @@
 package com.andy.feign.client;
 
 import com.andy.feign.entity.User;
-import feign.Param;
-import feign.RequestLine;
+import com.andy.feign.pojo.UserAddVO;
+import com.andy.feign.pojo.UserEditVO;
+import com.andy.feign.pojo.UserVO;
+import feign.*;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Leone
  * @since 2018-03-13
  **/
-@FeignClient(value = "spring-cloud-user")
+@FeignClient(value = "user"/*, url = "http://127.0.0.1/user"*/)
 public interface UserFeignClient {
 
-//	不支持getMapping和postMapping并且pathVariable的value属性必须填写
-//	@RequestMapping(value="/getUser/{id}",method=RequestMethod.GET)
-//    User getUser(@PathVariable("id")int id);
-//
-//	@RequestMapping(value="/postUser",method=RequestMethod.POST)
-//	User postUser(@RequestBody User user);
-
     @RequestLine("GET /user/{id}")
-    User user(@Param("id") Integer id);
+    User find(@Param("id") Long id, @HeaderMap Map<String, String> header);
 
     @RequestLine("GET /user/list")
+    @Headers("name-Type: james")
     List<User> list();
 
-    @RequestLine("POST /user")
-    User user(@RequestBody User user);
+    @RequestLine("PUT /user")
+    UserVO update(@RequestBody UserEditVO user);
 
+    @RequestLine("POST /user")
+    UserVO save(@RequestBody UserAddVO user);
+
+    @RequestLine("DELETE /user")
+    void delete(@QueryMap Map<String, Long> query);
+
+    @RequestLine("POST /user/upload")
+    String upload(MultipartFile file);
 
 }
