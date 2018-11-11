4e3db176e513eb4f64395c50e7bc331c7cf7303d
leone <exklin@gmail.com>
2018-11-11 11:57:07 +0800

    feat(all):update hystrix service

--- spring-cloud-hystrix/src/main/java/com/andy/hystrix/config/HystrixClientFallbackFactory.java
+++ spring-cloud-hystrix/src/main/java/com/andy/hystrix/config/HystrixClientFallbackFactory.java
@@ -1,13 +1,18 @@
 package com.andy.hystrix.config;
 
 import com.andy.hystrix.entity.User;
+import com.andy.hystrix.pojo.UserAddVO;
+import com.andy.hystrix.pojo.UserEditVO;
+import com.andy.hystrix.pojo.UserVO;
 import feign.hystrix.FallbackFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -15,24 +20,42 @@ import java.util.List;
  * @author Leone
  * @since 2018-02-13
  **/
-@Slf4j
-@Component
-public class HystrixClientFallbackFactory implements FallbackFactory<UserFeignClient1> {
-
-    @Override
-    public UserFeignClient1 create(Throwable cause) {
-        return new UserFeignClient1() {
-            @Override
-            public User user(Integer userId) {
-                log.info("进入fallback方法");
-                return new User(12, new Date(), "james", "password", "15687793324");
-            }
-
-            @Override
-            public List<User> list() {
-                log.info("进入fallback方法");
-                return new ArrayList<>();
-            }
-        };
-    }
-}
+//@Slf4j
+//@Component
+//public class HystrixClientFallbackFactory implements FallbackFactory<UserFeignClient> {
+//
+//    @Override
+//    public UserFeignClient create(Throwable cause) {
+//        return new UserFeignClient() {
+//            @Override
+//            public User find(Long id) {
+//                return null;
+//            }
+//
+//            @Override
+//            public List<User> list() {
+//                return null;
+//            }
+//
+//            @Override
+//            public UserVO update(UserEditVO user) {
+//                return null;
+//            }
+//
+//            @Override
+//            public UserVO save(UserAddVO user) {
+//                return null;
+//            }
+//
+//            @Override
+//            public void delete(Map<String, Long> query) {
+//
+//            }
+//
+//            @Override
+//            public String upload(MultipartFile file) {
+//                return null;
+//            }
+//        };
+//    }
+//}
---
+++ spring-cloud-hystrix/src/main/java/com/andy/hystrix/config/UserFeignClient.java
@@ -0,0 +1,48 @@
+package com.andy.hystrix.config;
+
+import com.andy.hystrix.entity.User;
+import com.andy.hystrix.pojo.UserAddVO;
+import com.andy.hystrix.pojo.UserEditVO;
+import com.andy.hystrix.pojo.UserVO;
+import feign.Headers;
+import feign.Param;
+import feign.QueryMap;
+import feign.RequestLine;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * <p>
+ *
+ * @author Leone
+ * @since 2018-02-16
+ **/
+@FeignClient(value = "user"/*, url = "http://127.0.0.1", fallbackFactory = HystrixClientFallbackFactory.class*/)
+public interface UserFeignClient {
+
+    @RequestLine("GET /user/{id}")
+    User find(@Param("id") Long id);
+
+    @RequestLine("GET /user/list")
+    @Headers("name-Type: james")
+    List<User> list();
+
+    @RequestLine("PUT /user")
+    UserVO update(@RequestBody UserEditVO user);
+
+    @RequestLine("POST /user")
+    UserVO save(@RequestBody UserAddVO user);
+
+    @RequestLine("DELETE /user")
+    void delete(@QueryMap Map<String, Long> query);
+
+    @RequestLine("POST /user/upload")
+    String upload(MultipartFile file);
+
+}
+
+
