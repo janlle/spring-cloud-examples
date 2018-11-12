44e2630019179293a1430bd7f60115351a787d11
leone <exklin@gmail.com>
2018-11-12 14:07:47 +0800

    feat(all):update hystrix

--- spring-cloud-hystrix/src/main/java/com/andy/HystrixApplication.java
+++ spring-cloud-hystrix/src/main/java/com/andy/HystrixApplication.java
@@ -1,13 +1,9 @@
 package com.andy;
 
-import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -20,8 +16,6 @@ import org.springframework.web.client.RestTemplate;
  **/
 @EnableHystrix
 @EnableFeignClients
-@EnableCircuitBreaker
-@EnableHystrixDashboard
 @SpringCloudApplication
 public class HystrixApplication {
 
@@ -29,15 +23,15 @@ public class HystrixApplication {
         SpringApplication.run(HystrixApplication.class, args);
     }
 
-    @Bean
-    public ServletRegistrationBean getServlet() {
-        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
-        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
-        registrationBean.setLoadOnStartup(1);
-        registrationBean.addUrlMappings("/hystrix.stream");
-        registrationBean.setName("HystrixMetricsStreamServlet");
-        return registrationBean;
-    }
+//    @Bean
+//    public ServletRegistrationBean getServlet() {
+//        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+//        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+//        registrationBean.setLoadOnStartup(1);
+//        registrationBean.addUrlMappings("/hystrix.stream");
+//        registrationBean.setName("HystrixMetricsStreamServlet");
+//        return registrationBean;
+//    }
 
     @Bean
     @LoadBalanced
--- spring-cloud-hystrix/src/main/java/com/andy/hystrix/config/UserFeignClient.java
+++ spring-cloud-hystrix/src/main/java/com/andy/hystrix/config/UserFeignClient.java
@@ -4,10 +4,7 @@ import com.andy.hystrix.entity.User;
 import com.andy.hystrix.pojo.UserAddVO;
 import com.andy.hystrix.pojo.UserEditVO;
 import com.andy.hystrix.pojo.UserVO;
-import feign.Headers;
-import feign.Param;
-import feign.QueryMap;
-import feign.RequestLine;
+import feign.*;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,28 +18,28 @@ import java.util.Map;
  * @author Leone
  * @since 2018-02-16
  **/
-@FeignClient(value = "mc-user", url = "http://127.0.0.1:8001"/*, fallbackFactory = HystrixClientFallbackFactory.class*/)
-public interface UserFeignClient {
-
-    @RequestLine("GET /user/{id}")
-    User find(@Param("id") Long id);
-
-    @RequestLine("GET /user/list")
-    @Headers("name-Type: james")
-    List<User> list();
-
-    @RequestLine("PUT /user")
-    UserVO update(@RequestBody UserEditVO user);
-
-    @RequestLine("POST /user")
-    UserVO save(@RequestBody UserAddVO user);
-
-    @RequestLine("DELETE /user")
-    void delete(@QueryMap Map<String, Long> query);
-
-    @RequestLine("POST /user/upload")
-    String upload(MultipartFile file);
-
-}
+//@FeignClient(value = "mc-user"/*, url = "http://127.0.0.1/user"*//*, configuration = FeignConfiguration1.class*/)
+//public interface UserFeignClient {
+//
+//    @RequestLine("GET /user/{id}")
+//    User find(@Param("id") Long id);
+//
+//    @RequestLine("GET /user/list")
+//    @Headers("name-Type: james")
+//    List<User> list();
+//
+//    @RequestLine("PUT /user")
+//    UserVO update(@RequestBody UserEditVO user);
+//
+//    @RequestLine("POST /user")
+//    UserVO save(@RequestBody UserAddVO user);
+//
+//    @RequestLine("DELETE /user")
+//    void delete(@QueryMap Map<String, Long> query);
+//
+//    @RequestLine("POST /user/upload")
+//    String upload(MultipartFile file);
+//
+//}
 
 
