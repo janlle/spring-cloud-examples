dcf7b76819061934d36aa5c90be52545cf7de0a7
leone <exklin@gmail.com>
2019-03-30 12:39:32 +0800

    :sparkles: update config server

--- spring-cloud-config-server/src/main/java/com/andy/config/ConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/andy/config/ConfigServerApplication.java
@@ -4,27 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Leone
  * @since 2017-12-07
  **/
-@RestController
 @EnableConfigServer
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
-        System.out.println(System.getProperty("user.dir"));
     }
-
-    @RequestMapping("/")
-    public String home() {
-        return "Hello World!";
-    }
-
 }
