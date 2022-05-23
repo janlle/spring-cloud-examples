7a4ee857e751efe7faf0534713d19fb592136b73
leone <exklin@gmail.com>
2022-05-23 19:46:09 +0800

    update

--- spring-cloud-consul/src/main/java/com/leone/cloud/consul/ConsulApplication.java
+++ spring-cloud-consul/src/main/java/com/leone/cloud/consul/ConsulApplication.java
@@ -4,16 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * <p>
+ * <p>注册服务到consul上
  *
- * @author Leone
+ * @author leone
  * @since 2018-10-26
  **/
 @SpringBootApplication
 public class ConsulApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ConsulApplication.class, args);
     }
-
 }
