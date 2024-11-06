1cedc65b0a052ccb81f3a8f2c824c66676a749a3
leone <exklin@gmail.com>
2024-11-06 15:38:35 +0800

    update

--- spring-cloud-config-server/src/main/java/com/leone/cloud/config/filter/WebHookFilter.java
+++ spring-cloud-config-server/src/main/java/com/leone/cloud/config/filter/WebHookFilter.java
@@ -1,10 +1,10 @@
 package com.leone.cloud.config.filter;
 
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.core.annotation.Order;
 
-import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 /**
